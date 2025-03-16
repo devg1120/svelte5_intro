32d19183eb72a3197db3baa5f7477bc49a4d3f22
devg1120 <devg1120@gmail.com>
2025-03-16 11:11:23 +0900

    up

---
+++ kit5-data-load/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script >
+  const { children } = $props();
+
+</script>
+<nav>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+  <a href="/blog">blog</a>
+
+</nav>
+
+{@render children()}
+
---
+++ kit5-data-load/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+
+<h1>home</h1>
+<p>this is the <strong>Home</strong> page.</p>
+
